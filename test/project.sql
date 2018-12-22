7d199150186e85265d0e244758a2bf41cccebfeb
liyongli <469337451@qq.com>
2018-12-22 17:03:14 +0800

    0

--- test/project.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO 表名称 VALUES (值1, 值2,....)
-
-INSERT INTO Project VALUE(1,'区块链征信系统联盟链'，'区块链征信系统联盟链',0,'2018-12-11','/usr/hubweb/app/dist/img/1.jpg')
-INSERT INTO Project VALUE(2,'互联网检测与取证技术'，'互联网检测与取证技术',0,'2018-12-11','/usr/hubweb/app/dist/img/2.jpg')
-INSERT INTO Project VALUE(3,'区块链保全系统私链'，'区块链保全系统私链',0,'2018-12-11','/usr/hubweb/app/dist/img/3.jpg')
-
